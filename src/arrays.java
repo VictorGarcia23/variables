abb598561fc78d2519c0055443d0f85ce1bdcdb1
VictorGarcia23 <garciavictor1111@gmail.com>
2020-07-08 12:18:29 -0400

    soluciones finales

--- src/arrays.java
+++ src/arrays.java
@@ -2,19 +2,28 @@ import java.util.Scanner;
 
 public class arrays {
     public static void main(String[] args) {
-        int[] unArray = new int[1];
+        int[] unArray = new int[3];
         Scanner input = new Scanner(System.in);
-        System.out.println("Dame un número de 3 cifras.");
+        System.out.println("Dame las unidades: ");
+        int unidades = input.nextInt();
+        System.out.println("Dame las decenas: ");
+        int decenas = input.nextInt();
+        System.out.println("Dame las centenas: ");
+        int centenas = input.nextInt();
 
-        int entero = input.nextInt();
+        unArray[0] = unidades;
+        unArray[1] = decenas;
+        unArray[2] = centenas;
 
-
-        if (entero > 100 && entero<999){
-            unArray[0] = entero;
-            System.out.println(unArray[0]);
+        for(int i = unArray.length -1; i >= 0; i--){
+            unArray[i] += 1;
+            System.out.print(unArray[i]);
         }
-        if (entero <100 || entero > 999){
-            System.out.println("necesito un número de 3 cifras.");
+
+
+
+
+
         }
     }
-}
+
