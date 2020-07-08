700e81ec6402354d076ef694361d4de5ef18c5b8
VictorGarcia23 <garciavictor1111@gmail.com>
2020-07-08 12:28:40 -0400

    minimizado arrays

--- src/arrays.java
+++ src/arrays.java
@@ -4,16 +4,11 @@ public class arrays {
     public static void main(String[] args) {
         int[] unArray = new int[3];
         Scanner input = new Scanner(System.in);
-        System.out.println("Dame las unidades: ");
-        int unidades = input.nextInt();
-        System.out.println("Dame las decenas: ");
-        int decenas = input.nextInt();
-        System.out.println("Dame las centenas: ");
-        int centenas = input.nextInt();
+        for (int i = 0; i< unArray.length;i++){
+            System.out.println("introduce un valor");
+            unArray[i] = input.nextInt();
+        }
 
-        unArray[0] = unidades;
-        unArray[1] = decenas;
-        unArray[2] = centenas;
 
         for(int i = unArray.length -1; i >= 0; i--){
             unArray[i] += 1;
